c061c9b49ccefa4105577932eac7cdaeff14617b
Jegadeesan <jegadeesan5vidya>
2026-06-06 15:06:37 +0800

    Environment setup

---
+++ main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+// import os
+
+
+def main():
+    print("Hello from langchain-course!")
+    //print("OPEN --> " + os.environ.get("OPENAI_API_KEY"))
+    //print("GIT --> " + os.environ.get("GIT_KEY"))
+
+if __name__ == "__main__":
+    main()
